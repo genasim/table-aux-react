176781f4b1a83b5e5ec7d3b722cf1e9a9f5ca2ea
Genadi Kolev <genadimkolev@gmail.com>
2023-08-31 15:06:05 +0300

    feat: Clicking on cells changes bg color, persistant

--- frontend/src/components/Cell.jsx
+++ frontend/src/components/Cell.jsx
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useUpdateDocMutation } from '../features/apiSlice';
+
+const getKeyByValue = (object, value) => {
+    const foundKey = Object.keys(object).find(key => object[key] === value);
+    return foundKey || null; // Value not found in the object
+}
 
 function Cell({ value, record }) {
     const [updateDocument] = useUpdateDocMutation()
 
-    const getKeyByValue = (object, value) => {
-        const foundKey = Object.keys(object).find(key => object[key] === value);
-        return foundKey || null; // Value not found in the object
-    }
+    const [key, setKey] = useState(null)
+    const [isMarked, setIsMarked] = useState(false)
+
+    useEffect(() => {
+        const key = getKeyByValue(record, value)
+        setKey(key)
+
+        const marked = record.marked.includes(key)
+        setIsMarked(marked)
+    }, [])
 
     const handleCellClick = async () => {
-        const key = getKeyByValue(record, value)
         const markedSet = new Set([...record.marked, key])
+        setIsMarked(true)
 
         const updatedDoc = { ...record, marked: [...markedSet] }
         await updateDocument(updatedDoc)
@@ -19,7 +30,7 @@ function Cell({ value, record }) {
 
     return (
         <td
-            className='border border-neutral-400 p-3 hover:bg-slate-300 hover:cursor-pointer'
+            className={`border border-neutral-400 p-3  hover:cursor-pointer ${isMarked ? 'hover:bg-green-500 bg-green-400' : 'hover:bg-slate-300'}`}
             onClick={handleCellClick}>
             {value}
         </td>
